44e369c6ec5993cf4d8b22af3ec1be6583d49da7
XingSong Lin <linxingsong@gmail.com>
2018-04-02 19:01:23 -0400

    register...working on

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './routing.module';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { RegisterComponent } from './component/register/register.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const path = require('path');
 const config_url = require('./config/database');
 
 const app = express();
+const path = require('path');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+const authentication  = require('./routes/authenticaton');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config_url.url, [config_url.db], (err)=>{
+mongoose.connect(config_url.url, (err)=>{
   if(err){
     console.log("Could not connect to database: ", err);
   }else{
-    console.log("Connected to database: "+ config_url.db);
+    console.log("Connected to database.");
   }
 });
 
 app.use(express.static(__dirname+'/client/dist/'));
+//body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/authentication', authentication);
 
 app.get('/', (req, res)=>{
   res.sendFile(path.join(__dirname+'/client/dist/index.html'));
