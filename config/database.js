b6b662843ac90ced1fc98009af6bb45983d70167
XingSong Lin <linxingsong@gmail.com>
2018-04-10 19:30:31 -0400

    cleaning up

--- config/database.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto').randomBytes(256).toString('hex');
-
-module.exports = {
-    url:  'mongodb://eric:Joe273745421@ds131329.mlab.com:31329/blog',
-    secret: crypto
-  } 
